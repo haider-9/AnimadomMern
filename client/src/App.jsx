c21989d4aca7561d690ef3f1a4c716fc882d894b
haider-9 <haiderahmad352@gmail.com>
2025-02-27 17:11:22 +0500

    intialized project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <Router> 
       <Navbar />
       <Routes>
-        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>
-        <Link to="/about">About</Link>
+    <div className="backdrop-blur-sm bg-white/30 w-full  p-2 flex items-center justify-evenly rounded-full my-3 mx-auto ">
+      <div className="size-20 rounded-full overflow-hidden logo  ">
+        <img
+          src="https://dummyimage.com/500x500"
+          alt=""
+          className="object-center object-cover"
+        />
+      </div>
+      <div className="flex items-center gap-4 links">
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
