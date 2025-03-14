5a295f4f9bd91701ccd4305cd2bcf109f546f9de
haider-9 <haiderahmad352@gmail.com>
2025-03-14 16:55:31 +0500

    seperated frontend

--- server/lib/ApiError.js
+++
@@ -1,9 +0,0 @@
-
-// API Error class for standardized error responses
-export default class ApiError extends Error {
-    constructor(statusCode, message) {
-        super(message);
-        this.statusCode = statusCode;
-        this.success = false;
-    }
-}
